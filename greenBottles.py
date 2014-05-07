ca96176db0f61e7095b96680b3d4074cae90a30e
bmurkin7089 <bmurkin7089@sohamcollege.org.uk>
2014-05-07 09:43:30 +0100

    Update greenBottles.py
    I made it loop by using a for loop and added a 0.5 second interval between each paragraph.

--- greenBottles.py
+++ greenBottles.py
@@ -1,4 +1,10 @@
-print("10 green bottles, hanging on the wall")
-print("10 green bottles, hanging on the wall")
-print("And if 1 green bottle should acidentally fall,")
-print("They'll be 9 green bottles hanging on the wall.")
+import time
+
+bottles = 10
+
+for x in range(bottles,0,-1):
+  print("{0} green bottles, hanging on the wall".format(x))
+  print("{0} green bottles, hanging on the wall".format(x))
+  print("And if 1 green bottle should acidentally fall,")
+  print("They'll be {0} green bottles hanging on the wall.".format(x-1))
+  time.sleep(0.5)
